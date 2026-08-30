924977e79911db0a0f9eb79792e01b1440f87379
Vaishnavi Pawar <vaishnavi.22211188@viit.ac.in>
2026-08-30 21:22:57 +0530

    Fix frontend cart calculation and Docker deployment

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -49,12 +49,17 @@ const StoreContextProvider = (props) => {
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
+
     for (const item in cartItems) {
       if (cartItems[item] > 0) {
-        let itemInfo = food_list.find((product) => product._id === item);
-        totalAmount += itemInfo.price * cartItems[item];
+        const itemInfo = food_list.find((product) => product._id === item);
+
+        if (itemInfo) {
+          totalAmount += itemInfo.price * cartItems[item];
+        }
       }
     }
+
     return totalAmount;
   };
 
